a7a276f5a643e41ed7ae1836ced141f1103ba12b
codetestinglabs <codetestinglab@gmail.com>
2024-01-22 00:12:01 +0600

    Create Subscription Model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // One who is subscribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // One to whom 'subscriber' is subscribing
+        ref: "User"
+    }
+}, { timestamps: true })
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
